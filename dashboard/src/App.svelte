46270f7ccf2bbc454767f8200cc67c2c6def5d7f
Jim Pivarski <jpivarski@gmail.com>
2025-10-17 17:36:08 -0500

    finished defining the basemaps

--- dashboard/src/App.svelte
+++ dashboard/src/App.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { MapLibre } from 'svelte-maplibre';
+  import mapStyle from './map-style.json';
 
-  let leftWidth = window.innerWidth / 2;
+  let leftWidth = 0.7 * window.innerWidth;
   let dragging = false;
 
   function startDrag(e: MouseEvent) {
@@ -32,21 +33,82 @@
     };
   });
 
+  let map;
+  function handleOnLoad(theMap) {
+    map = theMap;
+  }
+
+  function toggleBaselayer(layer) {
+    if (layer == "basic") {
+      map.setPaintProperty("aerial_2013", "raster-opacity", 0);
+      map.setPaintProperty("aerial_2021", "raster-opacity", 0);
+    }
+    else if (layer == "aerial_2013") {
+      map.setPaintProperty("aerial_2013", "raster-opacity", 1);
+      map.setPaintProperty("aerial_2021", "raster-opacity", 0);
+    }
+    else if (layer == "aerial_2022") {
+      map.setPaintProperty("aerial_2013", "raster-opacity", 0);
+      map.setPaintProperty("aerial_2021", "raster-opacity", 1);
+    }
+  }
+
 </script>
 
 <div class="whole-page"> 
   <div class="left-half" style="width: {leftWidth}px;">
-   <MapLibre 
-      center={[50,20]}
-      zoom={7}
+    <MapLibre
+      center={[-122.88, 38.46]}
+      zoom={9}
       class="map"
       standardControls
-      style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      style={mapStyle}
+      onload={handleOnLoad}
+      onzoomend={({ target: map }) => {
+          if (map.getZoom() < 14) {
+              toggleBaselayer("basic");
+              document.getElementById("baselayer_basic").checked = true;
+              document.getElementById("baselayer_aerial_2013").disabled = true;
+              document.getElementById("baselayer_aerial_2021").disabled = true;
+              document.querySelector('label[for="baselayer_aerial_2013"]').classList.add("disabled");
+              document.querySelector('label[for="baselayer_aerial_2021"]').classList.add("disabled");
+              document.getElementById("zoom_in_message").style.display = "block";
+          }
+          else {
+              document.getElementById("baselayer_aerial_2013").disabled = false;
+              document.getElementById("baselayer_aerial_2021").disabled = false;
+              document.querySelector('label[for="baselayer_aerial_2013"]').classList.remove("disabled");
+              document.querySelector('label[for="baselayer_aerial_2021"]').classList.remove("disabled");
+              document.getElementById("zoom_in_message").style.display = "none";
+          }
+      }}
       />
   </div>
   <div class="divider" on:mousedown={startDrag}></div>
   <div class="right-half" style="width: calc(100vw - 10px - {leftWidth}px);">
-    <p>Hello!</p>
+    <div>
+      <h3>Map layers</h3>
+      <div>
+        <label><input type="radio" name="baselayer" id="baselayer_basic" checked on:change={
+                      (e) => toggleBaselayer("basic")
+          }> Basic topographic map</label> (<a href="https://github.com/nst-guide/osm-liberty-topo">from here</a>)
+      </div>
+      <div>
+        <label for="baselayer_aerial_2013" class="disabled"><input type="radio" name="baselayer" id="baselayer_aerial_2013" disabled on:change={
+                      (e) => toggleBaselayer("aerial_2013")
+          }> 2013 aerial photography</label>
+        (<a href="https://www.arcgis.com/apps/mapviewer/index.html?url=https://socogis.sonomacounty.ca.gov/image/rest/services/Rasters/Ortho_SoCo_SonomaVeg_2013_WM/ImageServer&source=sd">Sonoma GIS</a>,
+        <a href="https://www.arcgis.com/home/item.html?id=a5fc12e9c4324663bafde942a7d1e1d3">through Esri</a>)
+      </div>
+      <div>
+        <label for="baselayer_aerial_2021" class="disabled"><input type="radio" name="baselayer" id="baselayer_aerial_2021" disabled on:change={
+                      (e) => toggleBaselayer("aerial_2022")
+          }> 2021 aerial photography</label>
+        (<a href="https://gis.sonomacounty.ca.gov/datasets/dc026cbfb9884d51a65dae1846bf76a5/explore?location=38.472153%2C-122.943650%2C10.18">Sonoma GIS</a>,
+        <a href="https://www.arcgis.com/home/item.html?id=0c361a688a5a453487021132c878e870">through Esri</a>)
+      </div>
+      <div id="zoom_in_message" style="margin-left: 25px;">(Zoom in to enable aerial photography.)</div>
+    </div>
   </div>
 </div>
 
@@ -92,6 +154,11 @@
     padding: 1rem;
     box-sizing: border-box;
     transition: none;
+  }
+
+  :global(label.disabled) {
+    color: #aaa;
+    cursor: not-allowed;
   }
 
   :global(.map) {
