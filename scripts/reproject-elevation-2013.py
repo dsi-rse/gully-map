8de45db0d384f08be33a74d3598c4ac05c140ad2
Jim Pivarski <jpivarski@gmail.com>
2025-10-23 01:32:56 -0500

    for the 2013 PMTiles, merge the GeoTIFFs before reprojecting

--- scripts/reproject-elevation-2013.py
+++ scripts/reproject-elevation-2013.py
@@ -1,22 +1,21 @@
-import glob
-import threading
-import queue
-
 import numpy as np
 import rasterio.warp
-
-NUM_THREADS = 4  # limited by RAM
 
 NODATA = np.finfo(np.float32).max
 FEET_PER_METER = 3.2808398950131
 
-print("reading 2022 file")
-with rasterio.open("SONOMA_DTM_2020.tif") as src:
+# gdal_merge.py -o merged_2013.tif -of GTiff -n -9999 -a_nodata -9999 bare-earth-hydroflattened-2013/*.tif
+
+print("reading")
+with rasterio.open("merged_2013.tif") as src:
+    src_data = src.read(1)
     src_crs = src.crs
     src_transform = src.transform
     src_width = src.width
     src_height = src.height
     src_bounds = src.bounds
+
+print(f"{np.count_nonzero(src_data == -9999) = } and {src_data.size = }")
 
 dst_crs = "EPSG:3857"
 
@@ -24,70 +23,25 @@ dst_transform, dst_width, dst_height = rasterio.warp.calculate_default_transform
     src_crs, dst_crs, src_width, src_height, *src_bounds
 )
 
-scratch = []
-for i in range(NUM_THREADS):
-    print(f"allocating array {i + 1} of {NUM_THREADS}")
-    scratch.append(np.full((dst_height, dst_width), NODATA, dtype=np.float32))
+print("reprojecting")
+tmp_data = np.empty((dst_height, dst_width), dtype=np.float32)
+rasterio.warp.reproject(
+    source=src_data,
+    destination=tmp_data,
+    src_transform=src_transform,
+    src_crs=src_crs,
+    dst_transform=dst_transform,
+    dst_crs=dst_crs,
+    resampling=rasterio.enums.Resampling.bilinear,
+    src_nodata=-9999,
+    dst_nodata=NODATA,
+)
+del src_data
 
-filenames = sorted(glob.glob("bare-earth-hydroflattened-2013/*.tif"))
+print("converting from feet into meters")
+tmp_data[tmp_data != NODATA] /= FEET_PER_METER
 
-tasks = queue.Queue()
-for fileindex, filename in enumerate(filenames):
-    tasks.put((fileindex, filename))
-
-for _ in range(NUM_THREADS):
-    tasks.put(None)
-
-def worker(index):
-    while True:
-        task = tasks.get()
-        if task is None:
-            break
-
-        fileindex, filename = task
-        print(f"worker {index} takes {fileindex + 1} of {len(filenames)}: {filename}")
-
-        with rasterio.open(filename) as file:
-            file_data = file.read(1) / FEET_PER_METER
-            file_transform = file.transform
-            file_crs = file.crs
-            file_nodata = file.nodata
-
-        rasterio.warp.reproject(
-            source=file_data,
-            destination=scratch[index],
-            src_transform=file_transform,
-            src_crs=file_crs,
-            dst_transform=dst_transform,
-            dst_crs=dst_crs,
-            resampling=rasterio.enums.Resampling.bilinear,
-            src_nodata=file_nodata,
-            dst_nodata=NODATA,
-            init_dest_nodata=True,
-        )
-
-        del file_data
-
-    print(f"worker {index} is done")
-
-threads = [threading.Thread(target=worker, args=(i,)) for i in range(NUM_THREADS)]
-
-for thread in threads:
-    thread.start()
-
-for thread in threads:
-    thread.join()
-
-print("combining scratch spaces")
-tmp_data = scratch[0]
-for i, array in enumerate(scratch[1:]):
-    selection = (array != NODATA)
-    print(f"    {i + 1} into 0; selection has {np.count_nonzero(selection)} pixels")
-    tmp_data[selection] = array[selection]
-
-del scratch
-
-print(f"{np.count_nonzero(tmp_data != NODATA) = } pixels")
+print(f"{np.count_nonzero(tmp_data == NODATA) = } and {tmp_data.size = }")
 
 dst_data = tmp_data.view(np.uint8).reshape((dst_height, dst_width, 4))
 dst_data = np.transpose(dst_data, (2, 0, 1))
@@ -111,3 +65,5 @@ with rasterio.open(
     dst.write(dst_data, [1, 2, 3, 4])
 
 print("done")
+
+# rio pmtiles elevation-2013.tif elevation-2013.pmtiles --format PNG --zoom-levels 17..17 --tile-size 256
