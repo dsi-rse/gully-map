69c59326b4a6c4d3c005ed9b705838b9e1ae8bcd
Jim Pivarski <jpivarski@gmail.com>
2025-10-28 16:15:20 -0500

    parallelize parts of the elevation-difference process

--- scripts/elevation-difference-contours.py
+++ scripts/elevation-difference-contours.py
@@ -1,13 +1,54 @@
 import time
 
 import numpy as np
+import numba as nb
 import geopandas as gpd
 import rasterio.warp
 import scipy.ndimage
 import skimage
 import shapely
-import cv2
-import matplotlib as mpl
+
+@nb.jit(parallel=True)
+def float32_to_rgba(difference):
+    out = np.empty((4,) + difference.shape, dtype=np.uint8)
+    for i in nb.prange(difference.shape[0]):
+        for j in nb.prange(difference.shape[1]):
+            value = difference[i, j]
+            strength = min(abs(value) / 6, 1)
+            if value < 0:
+                # 0 -> #ffffff, 1 -> #6f00ff
+                out[0, i, j] = (1 - strength) * (0xff - 0x6f) + 0x6f  # 0 -> ff, 1 -> 6f
+                out[1, i, j] = (1 - strength) * (0xff - 0x00) + 0x00  # 0 -> ff, 1 -> 00
+                out[2, i, j] = 0xff                                   # 0 -> ff, 1 -> ff
+            else:
+                # 0 -> #ffffff, 1 -> #00a000
+                out[0, i, j] = (1 - strength) * (0xff - 0x00) + 0x00  # 0 -> ff, 1 -> 00
+                out[1, i, j] = (1 - strength) * (0xff - 0xa0) + 0xa0  # 0 -> ff, 1 -> a0
+                out[2, i, j] = (1 - strength) * (0xff - 0x00) + 0x00  # 0 -> ff, 1 -> 00
+            # full alpha channel
+            out[3, i, j] = 0xff
+    return out
+
+@nb.jit(parallel=True)
+def convolve2d(image, kernel):
+    output = np.empty(image.shape, image.dtype)
+    imageI, imageJ = image.shape
+    kernelI, kernelJ = kernel.shape
+    halfI = kernelI // 2
+    halfJ = kernelJ // 2
+    for imi in nb.prange(imageI):
+        for imj in nb.prange(imageJ):
+            accumulate = 0
+            for ki in range(kernelI):
+                for kj in range(kernelJ):
+                    # boundary conditions that reflect
+                    i = abs(imi + ki - halfI)
+                    j = abs(imj + kj - halfJ)
+                    i = i if i < imageI else 2 * imageI - i - 1
+                    j = j if j < imageJ else 2 * imageJ - j - 1
+                    accumulate += image[i, j] * kernel[ki, kj]
+            output[imi, imj] = accumulate
+    return output
 
 starttime = time.time()
 
@@ -99,17 +140,11 @@ with rasterio.open("elevation-2013.tif") as file:
 print(int(time.time() - starttime), "computing difference", flush=True)
 difference = elevation2022 - elevation2013
 
-print(int(time.time() - starttime), "clamping differences at +-6 meters", flush=True)
-np.minimum(difference, 6, out=difference)
-np.maximum(difference, -6, out=difference)
+del elevation2022
+del elevation2013
 
 print(int(time.time() - starttime), "converting elevation differences into a colored image", flush=True)
-custom_cmap = mpl.colors.LinearSegmentedColormap.from_list(
-    "custom_cmap", ["#6f00ff", "#ffffff", "#00a000"], N=256
-)
-difference_image = np.transpose(
-    (custom_cmap(difference / 12 + 0.5) * 255).astype(np.uint8), [2, 0, 1]
-)
+difference_image = float32_to_rgba(difference)
 
 print(int(time.time() - starttime), "writing elevation-difference as an RGBA GeoTIFF", flush=True)
 with rasterio.open(
@@ -130,9 +165,18 @@ with rasterio.open(
 
 del difference_image
 
+print(int(time.time() - starttime), "clamping differences at +-6 meters", flush=True)
+np.minimum(difference, 6, out=difference)
+np.maximum(difference, -6, out=difference)
+
 print(int(time.time() - starttime), "blurring differences for smooth contours", flush=True)
+sigma = 3
+x, y = np.meshgrid(np.arange(-12, 13), np.arange(-12, 13))
+kernel = np.exp(-(x**2 + y**2) / 2 / sigma**2)
+kernel /= kernel.sum()
+blurred = convolve2d(difference, kernel)
 # blurred = scipy.ndimage.gaussian_filter(difference, 3)
-blurred = cv2.GaussianBlur(difference, (0, 0), 3)
+# blurred = cv2.GaussianBlur(difference, (0, 0), 3)
 
 levels = [float(x) for x in np.arange(-3, 3 + 1/3 - 1e-5, 1/3) if abs(x) > 1e-5]
 
